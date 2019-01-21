3d0f9b0c7d12cac6fc6d95174e325c8b3a7a716c
Jais Djurhuus-Kempel <jdk@combine.dk>
2019-01-21 15:38:45 +0200

    Added BC for class renaming

---
+++ src/ParcelShop.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * This file is part of the Lsv\GlsDk
+ */
+namespace Lsv\PdDk;
+
+/**
+ * Client
+ *
+ * @author Martin Aarhof <martin.aarhof@gmail.com>
+ * @deprecated
+ */
+class ParcelShop extends Client
+{
+}
